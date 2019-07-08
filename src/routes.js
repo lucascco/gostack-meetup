ed00063f1366e82a53d3fa2f8433d9fa5a44c819
Lucas C Correa <lucasccorrea@gmail.com>
2019-07-08 00:33:24 -0300

    add eslint, prettier and tools to enforce code style

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  res.json({title: 'Meetup'});
+  res.json({ title: 'Meetup' });
 });
 
 export default routes;
