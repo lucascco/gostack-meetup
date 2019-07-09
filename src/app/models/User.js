609c323e20aab19812c23ad83c24eef855703fcf
Lucas C Correa <lucasccorrea@gmail.com>
2019-07-08 23:11:08 -0300

    add model User

---
+++ src/app/models/User.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', user => {
+      if (user.password) {
+        user.password = 'password crypt';
+      }
+    });
+  }
+}
+
+export default User;
